9ccbe5ed89d232f495e5326c1e03cb5f365a0079
Edward Kmett <ekmett@gmail.com>
2014-08-02 19:23:08 -0400

    contravariant applicative composition

--- Data/Functor/Contravariant/Compose.hs
+++ Data/Functor/Contravariant/Compose.hs
@@ -15,13 +15,16 @@ module Data.Functor.Contravariant.Compose
   , ComposeCF(..)
   ) where
 
+import Control.Arrow
+import Control.Applicative
 import Data.Functor.Contravariant
+import Data.Functor.Contravariant.Applicative
 
 -- | Composition of two contravariant functors
 newtype Compose f g a = Compose { getCompose :: f (g a) }
 
 instance (Contravariant f, Contravariant g) => Functor (Compose f g) where
-    fmap f (Compose x) = Compose (contramap (contramap f) x)
+   fmap f (Compose x) = Compose (contramap (contramap f) x)
 
 -- | Composition of covariant and contravariant functors
 newtype ComposeFC f g a = ComposeFC { getComposeFC :: f (g a) }
@@ -32,6 +35,10 @@ instance (Functor f, Contravariant g) => Contravariant (ComposeFC f g) where
 instance (Functor f, Functor g) => Functor (ComposeFC f g) where
     fmap f (ComposeFC x) = ComposeFC (fmap (fmap f) x)
 
+instance (Applicative f, ContravariantApplicative g) => ContravariantApplicative (ComposeFC f g) where
+  conquer = ComposeFC $ pure conquer
+  divide abc (ComposeFC fb) (ComposeFC fc) = ComposeFC $ divide abc <$> fb <*> fc
+
 -- | Composition of contravariant and covariant functors
 newtype ComposeCF f g a = ComposeCF { getComposeCF :: f (g a) }
 
@@ -40,3 +47,9 @@ instance (Contravariant f, Functor g) => Contravariant (ComposeCF f g) where
 
 instance (Functor f, Functor g) => Functor (ComposeCF f g) where
     fmap f (ComposeCF x) = ComposeCF (fmap (fmap f) x)
+
+instance (ContravariantApplicative f, Applicative g) => ContravariantApplicative (ComposeCF f g) where
+  conquer = ComposeCF conquer
+  divide abc (ComposeCF fb) (ComposeCF fc) = ComposeCF $ divide ((fmap fst &&& fmap snd) . fmap abc) fb fc
+
+-- | Composition of contravariant and covariant functors
