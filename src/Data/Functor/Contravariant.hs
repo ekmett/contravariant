2df5e6011f53da4b9b7b15c00f904e9be14e49d6
Edward Kmett <ekmett@gmail.com>
2015-03-08 05:27:39 -0400

    fixity

--- src/Data/Functor/Contravariant.hs
+++ src/Data/Functor/Contravariant.hs
@@ -233,7 +233,7 @@ newtype Comparison a = Comparison { getComparison :: a -> a -> Ordering }
 -- apply its function argument to each input to each input to the
 -- comparison function.
 instance Contravariant Comparison where
-  contramap f g = Comparison $ getComparison g `on` f
+  contramap f g = Comparison $ on (getComparison g) f
 
 #ifdef MIN_VERSION_semigroups
 instance Semigroup (Comparison a) where
@@ -278,7 +278,7 @@ newtype Equivalence a = Equivalence { getEquivalence :: a -> a -> Bool }
 -- apply the contramapped function to each input to the equivalence
 -- relation.
 instance Contravariant Equivalence where
-  contramap f g = Equivalence $ getEquivalence g `on` f
+  contramap f g = Equivalence $ on (getEquivalence g) f
 
 #ifdef MIN_VERSION_semigroups
 instance Semigroup (Equivalence a) where
